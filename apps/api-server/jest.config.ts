498605bb4a07e9f3a9b14e1c50a9824f319612d8
nemanja-mudrinic-nimbus-tech <83245943+nemanja-mudrinic-nimbus-tech@users.noreply.github.com>
2023-09-04 09:28:27 +0200

    Cleaning up for UI (#4)

--- apps/api-server/jest.config.ts
+++ apps/api-server/jest.config.ts
@@ -3,21 +3,22 @@ module.exports = {
   testEnvironment: "node",
   collectCoverage: true,
   /**
-   * TODO: Once controllers are covered with unit tests include them
+   * TODO: Once controllers and repositories are covered with unit tests include them
    */
   collectCoverageFrom: [
     "src/api/**/*.{js,ts}",
     "!src/**/*.d.ts",
     "!src/api/**/*.openapi.{js,ts}",
     "!src/api/**/*.controller.{js,ts}",
+    "!src/api/**/*.repository.{js,ts}",
   ],
   testMatch: ["**/*.test.ts"],
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
     },
   },
   coverageReporters: ["json", "lcov", "text", "clover"],
